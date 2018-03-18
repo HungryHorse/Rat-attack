6b7756f9178d26008434b7651bf901051e940af2
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-03-18 04:31:08 +0000

    AI

--- 2D Game for AINT/Assets/Scripts/Enemy.cs
+++ 2D Game for AINT/Assets/Scripts/Enemy.cs
@@ -7,31 +7,50 @@ public class Enemy : MonoBehaviour
 
     public bool sawPlayer;
     public GameObject player;
+    public GameObject playerDirectionMonitor;
     public float adjustmentAngle = 0.0f;
 
     void Start()
     {
         player = GameObject.Find("Player");
-
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));
+        playerDirectionMonitor = GameObject.Find("PlayerDirectionMonitor");
     }
 
     void Update()
     {
-        Vector3 target = player.transform.position;
-        Vector3 difference = target - transform.position;
+        if (sawPlayer)
+        {
+            Vector3 target = player.transform.position;
+            Vector3 difference = target - transform.position;
 
-        difference.Normalize();
+            difference.Normalize();
 
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
-        transform.rotation = newRotation;
-        
-        RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, Vector2.up);
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+            transform.rotation = newRotation;
+        }
+        else
+        {
+            Vector3 target = player.transform.position;
+            Vector3 difference = target - transform.position;
+
+            difference.Normalize();
+
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+            playerDirectionMonitor.transform.rotation = newRotation;
+
+            sendRay(playerDirectionMonitor);
+        }
+    }
+
+    void sendRay(GameObject origin)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(origin.transform.position, origin.transform.up);
 
         if (hit.collider != null)
         {
-            if(hit.collider.tag == "Player")
+            if (hit.collider.tag == "Player")
             {
                 sawPlayer = true;
             }
