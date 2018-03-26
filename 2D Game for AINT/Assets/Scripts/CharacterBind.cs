7401d87c4fd252cd76e3218b6b18f9c157d74ae4
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-03-26 19:21:35 +0100

    Skill menu thing

---
+++ 2D Game for AINT/Assets/Scripts/CharacterBind.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBind : MonoBehaviour {
+    public GameObject Buttons;
+
+	// Use this for initialization
+	void Start () {
+        Buttons = GameObject.Find("Buttons");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.SetActive(Buttons.activeInHierarchy);
+	}
+}
