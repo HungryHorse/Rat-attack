dbf54490f6ac3741467eb0325b73a2a84db18a04
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-04-18 15:07:32 +0100

    Menu stuff

---
+++ 2D Game for AINT/Assets/Scripts/SlideAnimeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class SlideAnimeController : MonoBehaviour {
+
+    public Animator anim;
+    public Animator anim2;
+
+    public void GoRight()
+    {
+        anim.SetBool("Move", true);
+        anim2.SetBool("Move", true);
+    }
+
+    public void GoLeft()
+    {
+        anim.SetBool("Move", false);
+        anim2.SetBool("Move", false);
+    }
+}
